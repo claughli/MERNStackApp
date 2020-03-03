0b513b0df7d84d273d393560362d821887b1e046
Casey Laughlin <caseylaughlin1+github@gmail.com>
2020-03-03 16:59:33 -0600

    changed to hello world

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express()
 
-app.get('/', (req, res) => res.send("Hello World"));
+app.get('/', (req, res) => res.send("Hello world"));
 
 app.listen(3000, () => {
     console.log("Now listening on port 3000");
