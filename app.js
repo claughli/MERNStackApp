966ee1d90add17a811c46650f58aeeac63a2f4ba
Casey Laughlin <caseylaughlin1+github@gmail.com>
2020-03-04 14:28:51 -0600

    connection with database

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express()
 
+//establish a connection to the mongo db container
+mongoose.connect(process.env.MONGO_DB_URL, {useNewUrlParser: true}, function(err) {
+  if(err) {
+    console.log('error db connection failed: ' + err)
+  }
+  else {
+    console.log('Connection to DB successful')
+  }
+});
+
+
+
+//test to show base path works
 app.get('/', (req, res) => res.send("Hello new"));
 
+//set up port to listen on
 app.listen(3000, () => {
     console.log("Now listening on port 3000");
 })
